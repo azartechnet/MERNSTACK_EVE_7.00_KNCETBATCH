98aa2a771307f896b90bee4dd6e10e6d6953ad37
azartechnet <azartechnet@gmail.com>
2025-06-20 19:46:52 +0530

    updated

---
+++ crudproject/client/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home()
+{
+    return(
+        <div className="container mt-4">
+             <h1 className="text-center">HomePage</h1>
+             <p className="text-center">This is paragraph</p>
+        </div>
+    )
+}
+export default Home;
