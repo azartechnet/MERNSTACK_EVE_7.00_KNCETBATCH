3ccbe79a381d2135c892945c2647a3226760523c
azartechnet <azartechnet@gmail.com>
2025-07-05 19:31:51 +0530

    updated

--- crudproject/client/src/CrudPage.js
+++ crudproject/client/src/CrudPage.js
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Axios from 'axios';
 function CrudPage()
 {
 
     const [foodName,setFoodName]=useState("");
     const [description,setDescription]=useState("");
-
+    const [foodList,setFoodList]=useState([])
+     const [newFoodName,setNewFoodName]=useState("")
 
     //Add FoodData
 
@@ -23,6 +24,26 @@ function CrudPage()
                         }
                         );
     }
+      //get data
+    const fetchData=()=>{
+        Axios.get("http://localhost:3001/read").then((response)=>{
+            console.log(response.data)
+            setFoodList(response.data)
+        })
+    }
+     useEffect(()=>{
+        fetchData();
+    },[])
+
+     //update
+    const updateFood=(id)=>{
+        Axios.put('http://localhost:3001/update',{id,newFoodName})
+        .then(()=>fetchData())
+    }
+    //delete
+    const deleteFood=(id)=>{
+        Axios.delete(`http://localhost:3001/delete/${id}`).then(()=>fetchData())
+        }
     return(
 
         <div className='container'>
@@ -36,30 +57,30 @@ function CrudPage()
             <div className='mb-3'>
                 <button className='btn btn-primary' onClick={addFoodData}>AddFood</button>
             </div>
-            <table className='table table-bordered table-striped'>
-               <thead className='table-dark'>
-               <tr>
-                <th>FoodName</th>
-                <th>FoodDescription</th>
-                <th>Edit</th>
-                <th>Delete</th>
-               </tr>
-               </thead>
-               <tbody>
-                
-                   <tr>
-                     <td>Apple</td>
-                     <td>Its is good</td>
-                     <td>
-                        <input type="text" placeholder='updateFoodName'/>
-                        <button>Edit</button>
-                     </td>
-                     <td><button>Delete</button></td>
-                   </tr>
-               
-              
-               </tbody>
-            </table>
+             <h3 className="mt-4">Get Data From Database</h3>
+        <table className="table table-bordered table-striped">
+            <thead className="table-dark">
+                <tr>
+                    <th>Food Name</th>
+                    <th>Food Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+             {foodList.map((val,key)=>(
+                <tr key={key}>
+                    <td>{val.foodName}</td>
+                    <td>{val.description}</td>
+
+                    <td>
+                        <input type="text" placeholder="UpdateFoodName" onChange={(e)=>setNewFoodName(e.target.value)}/>
+                        <button className="btn btn-primary" onClick={()=>updateFood(val._id)}>Edit</button></td>
+                    <td><button className="btn btn-danger" onClick={()=>deleteFood(val._id)}>Delete</button></td>
+                </tr>
+             ))}
+            </tbody>
+        </table>
         </div>
 
     )
